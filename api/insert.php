461d7d4d8e08d7028f4c71b5b033158452f918d8
maplesift <a23050716@gmail.com>
2025-03-05 16:24:25 +0800

    01 l01db施工

---
+++ api/insert.php
@@ -0,0 +1,5 @@
+<?php include_once "db.php";
+
+if(!empty($_FILES['img']['tmp_name'])){
+    move_uploaded_file($_FILES['img']['tmp_name'],"../upload/".$_FILES['img']['name']);
+}
---
+++ modal/test.php
@@ -0,0 +1,15 @@
+<h2 class="cent">新增標題區圖片</h2>
+<form action="./api/insert.php" method="post" enctype="multipart/form-data">
+<table>
+    <td>
+        <tr>
+            
+        </tr>
+    </td>
+</table>
+<div class="cent">
+    <input type="submit" value="新增">
+    <input type="reset" value="重置">
+</div>
+
+</form>
