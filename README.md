5b57ab1f2821f397b2848c7a335dbfd2b74a28f7
maplesift <a23050716@gmail.com>
2025-03-08 22:38:22 +0800

    01 結束

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Z01_L
+
+- 難點:api: >insert >edit >edit sub
+- 前台各式function
+- 選單區:次選單
--- ref/01/db03_z01l1.sql
+++ ref/01/db03_z01l1.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主機： 127.0.0.1
--- 產生時間： 2025-03-06 09:32:42
+-- 產生時間： 2025-03-08 15:33:13
 -- 伺服器版本： 10.4.32-MariaDB
 -- PHP 版本： 8.2.12
 
@@ -108,6 +108,34 @@ INSERT INTO `image` (`id`, `img`, `sh`) VALUES
 -- --------------------------------------------------------
 
 --
+-- 資料表結構 `menu`
+--
+
+CREATE TABLE `menu` (
+  `id` int(11) NOT NULL,
+  `text` text NOT NULL,
+  `sh` int(1) NOT NULL DEFAULT 1,
+  `main_id` int(10) NOT NULL DEFAULT 0,
+  `link` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 傾印資料表的資料 `menu`
+--
+
+INSERT INTO `menu` (`id`, `text`, `sh`, `main_id`, `link`) VALUES
+(1, '管理登入', 1, 0, 'index.php?do=login'),
+(2, '網站首頁', 1, 0, 'index.php'),
+(3, '123', 1, 1, '321'),
+(4, '88', 1, 1, '88'),
+(5, '77', 1, 1, '77'),
+(6, '66', 1, 1, '66'),
+(7, 'hi', 1, 2, 'ih'),
+(8, 'sh', 1, 2, 'hs');
+
+-- --------------------------------------------------------
+
+--
 -- 資料表結構 `mvim`
 --
 
@@ -126,6 +154,30 @@ INSERT INTO `mvim` (`id`, `img`, `sh`) VALUES
 (2, '01C02.gif', 1),
 (3, '01C04.gif', 1),
 (5, '01C03.gif', 1);
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `news`
+--
+
+CREATE TABLE `news` (
+  `id` int(11) NOT NULL,
+  `text` text NOT NULL,
+  `sh` int(1) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 傾印資料表的資料 `news`
+--
+
+INSERT INTO `news` (`id`, `text`, `sh`) VALUES
+(4, '教師研習「世界公民生命園丁國內研習會」\r\n1.主辦單位：世界展望會\r\n2.研習日期：101年11月14日（三）～15日（四）\r\n3.詳情請參考：\r\nhttp://gc.worldvision.org.tw/seed.html。\r\n請線上報名。', 1),
+(5, '\r\n102年全國大專校院運動會\r\n「主題標語及吉祥物命名」\r\n網路票選活動\r\n一、活動期間：自10月25日起至11月4日止。\r\n二、相關訊息請上宜蘭大學首頁連結「102全大運在宜大」\r\n活動網址：http://102niag.niu.edu.tw/', 1),
+(6, '台灣亞洲藝術文化教育交流學會第一屆年會國際研討會\r\n活動日期：101年3月3～4日(六、日)\r\n活動主題：創造力、文化、全人教育\r\n有意參加者請至http://www.caaetaiwan.org下載報名表', 1),
+(7, '11月23日(星期五)將於彰化縣田尾鄉菁芳園休閒農場\r\n舉辦「高中職生涯輔導知能研習」\r\n中區學校每校至多2名\r\n以普通科、專業類科教師優先報名參加\r\n生涯規劃教師次之，參加人員公差假\r\n並核實派代課\r\n當天還有專車接送(8:35前在員林火車站集合)\r\n如此好康的機會，怎能錯過？！\r\n熱烈邀請師長們向輔導室(分機234)報名\r\n名額有限，動作要快！！\r\n報名截止日期：本周四 10月25日17:00前！', 1),
+(8, '台視百萬大明星節目辦理海選活動\r\n時間:101年10月27日下午13時\r\n地點:彰化', 1),
+(9, '國立故宮博物院辦理\r\n「商王武丁與后婦好 殷商盛世文化藝術特展」暨\r\n「赫赫宗周 西周文化特展」', 1);
 
 -- --------------------------------------------------------
 
@@ -166,7 +218,7 @@ CREATE TABLE `total` (
 --
 
 INSERT INTO `total` (`id`, `total`) VALUES
-(1, 54);
+(1, 55);
 
 --
 -- 已傾印資料表的索引
@@ -197,9 +249,21 @@ ALTER TABLE `image`
   ADD PRIMARY KEY (`id`);
 
 --
+-- 資料表索引 `menu`
+--
+ALTER TABLE `menu`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- 資料表索引 `mvim`
 --
 ALTER TABLE `mvim`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 資料表索引 `news`
+--
+ALTER TABLE `news`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -243,10 +307,22 @@ ALTER TABLE `image`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
+-- 使用資料表自動遞增(AUTO_INCREMENT) `menu`
+--
+ALTER TABLE `menu`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
 -- 使用資料表自動遞增(AUTO_INCREMENT) `mvim`
 --
 ALTER TABLE `mvim`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `news`
+--
+ALTER TABLE `news`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- 使用資料表自動遞增(AUTO_INCREMENT) `title`
